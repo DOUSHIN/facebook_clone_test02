f219df707d77af3b9f0baba0c9d5c2c366df8607
hiroyuki.nakagawa0325@gmail.com <hiroyuki.nakagawa0325@gmail.com>
2020-09-27 07:44:38 +0900

    「picture投稿機能」を実装中

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,2 +1,53 @@
 class PicturesController < ApplicationController
+
+  def new
+    @picture = Picture.new
+  end
+
+  def confirm
+    @picture = Picture.new(picture_params)
+  end
+
+  def index
+    @pictures = Picture.all
+  end
+
+  def show
+    @picture = Picture.find(params[:id])
+  end
+
+  def edit
+    @picture = Picture.find(params[:id])
+  end
+
+  def create
+    @picture = Picture.new(picture_params)
+    if params[:back]
+      render :new
+    else
+      if @picture.save
+        flash[:notice] = '投稿完了'
+        redirect_to picture_path(@picture.id)
+      else
+        render :new
+      end
+    end
+  end
+
+  def update
+    if params[:back]
+      render :new
+    else
+      @picture = Picture.find(params[:id])
+      @picture.update(picture_params)
+      flash[:hogehoge] = '編集完了'
+      redirect_to pictures_path
+    end
+  end
+
+  private
+  def picture_params
+    params.require(:picture).permit(:image, :image_cache)
+  end
+
 end
